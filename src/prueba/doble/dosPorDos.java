f0b25a76cd8ba84d38685b528d7e226d6fa68986
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2022-11-18 12:08:19 +0000

    Initial commit

---
+++ src/prueba/doble/dosPorDos.java
@@ -0,0 +1,157 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba.doble;
+
+/**
+ *
+ * @author ignac
+ */
+public class dosPorDos {
+    
+      static final int N = 2;
+      
+ //dbdrb
+    // Function to get cofactor of
+    // mat[p][q] in temp[][]. n is
+    // current dimension of mat[][]
+    static void getCofactor(int matriz[][], int temp[][],
+                            int p, int q, int n)
+    {
+        int i = 0, j = 0;
+ 
+        // Looping for each element of
+        // the matrix
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                // Copying into temporary matrix
+                // only those element which are
+                // not in given row and column
+                if (row != p && col != q)
+                {
+                    temp[i][j++] = matriz[row][col];
+                    // Row is filled, so increase
+                    // row index and reset col
+                    // index
+                    if (j == n - 1)
+                    {
+                        j = 0;
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+ 
+    /* Recursive function for finding determinant
+    of matrix. n is current dimension of mat[][]. */
+    static int determinant(int matriz[][], int n)
+    {
+        int D = 0; // Initialize result
+ 
+        // Base case : if matrix contains single
+        // element
+        if (n == 1)
+            return matriz[0][0];
+ 
+        // To store cofactors
+        int temp[][] = new int[N][N];
+ 
+        // To store sign multiplier
+        int sign = 1;
+ 
+        // Iterate for each element of first row
+        for (int f = 0; f < n; f++)
+        {
+            // Getting Cofactor of mat[0][f]
+            getCofactor(matriz, temp, 0, f, n);
+            D += sign * matriz[0][f]
+                 * determinant(temp, n - 1);
+ 
+            // terms are to be added with
+            // alternate sign
+            sign = -sign;
+        }
+ 
+        return D;
+    }
+    static void adjoint(int matriz[][],int [][]adj)
+{
+	if (N == 1)
+	{
+		adj[0][0] = 1;
+		return;
+	}
+
+	// temp is used to store cofactors of A[][]
+	int sign = 1;
+	int [][]temp = new int[N][N];
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			// Get cofactor of A[i][j]
+			getCofactor(matriz, temp, i, j, N);
+
+			// sign of adj[j][i] positive if sum of row
+			// and column indexes is even.
+			sign = ((i + j) % 2 == 0)? 1: -1;
+
+			// Interchanging rows and columns to get the
+			// transpose of the cofactor matrix
+			adj[j][i] = (sign)*(determinant(temp, N-1));
+		}
+	}
+}
+//
+// Function to calculate and store inverse, returns false if
+// matrix is singular
+static boolean inverse(int matriz[][], float [][]inverse)
+{
+	// Find determinant of A[][]
+	int det = determinant(matriz, N);
+	if (det == 0)
+	{
+		System.out.print("Singular matrix, can't find its inverse");
+		return false;
+	}
+
+	// Find adjoint
+	int [][]adj = new int[N][N];
+	adjoint(matriz, adj);
+
+	// Find Inverse using formula "inverse(A) = adj(A)/det(A)"
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			inverse[i][j] = adj[i][j]/(float)det;
+
+	return true;
+}
+
+    
+    
+    
+   static void display(int matriz[][])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			System.out.print(matriz[i][j]+ " ");
+		System.out.println();
+	}
+}
+static void display(float matriz[][])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			System.out.printf("%.6f ",matriz[i][j]);
+		System.out.println();
+	}
+}
+    
+}
