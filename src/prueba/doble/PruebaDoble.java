ee0a1fa826e63c4f99a713352bb88beda0862914
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2022-11-28 09:11:09 +0000

    AyUDAME BEEEEEEEEEEEEEEEERRni

--- src/prueba/doble/PruebaDoble.java
+++ src/prueba/doble/PruebaDoble.java
@@ -28,7 +28,7 @@ import static prueba.doble.dosPorDos.userInput;
         
         //WELCOMING PROCESS
         
-        int i, j, n ;
+        int i, j , n;
         String literales = "";
         char literal;
         Scanner in = new Scanner(System.in);
@@ -37,7 +37,7 @@ import static prueba.doble.dosPorDos.userInput;
         
         
 	float [][]inv = new float[n][n];
-        
+        int [][]conts = new int[n][1];
         int[][] matriz = new int[N][N+1];
         for(i=0; i<n; i++)
         {
@@ -56,21 +56,40 @@ import static prueba.doble.dosPorDos.userInput;
                 matriz[i][j] = Integer.parseInt(in.nextLine());
             }
              System.out.print("Ingrese la constante de la ecuaci\242n " + (i+1) + ": ");
-            matriz[i][n] = Integer.parseInt(in.nextLine());
+            conts[i][0] = Integer.parseInt(in.nextLine());
         }
+        
+        float result[][] = new float [n][1];
+        for (int e = 0; e < n; e++) 
+        {
+            for (int w = 0; w < 1; w++) 
+            {
+                for (int k = 0; k < n; k++)
+                {	 
+                    result[e][w] = result[e][w] + inv[e][k] * conts[k][e];
+                }
+            }
+        }
+        
+        System.out.println("The product is:");
+        for(int b=0; b<n; b++)
+        {
+            System.out.println(result[b][0] + " ");
+        }
+       
         
         
         System.out.print("Determinant " + "of the matrix is : " + determinant(matriz, n));
     
-        System.out.println("Input matrix is :\n");
-	displayComEcu(matriz);
-
-	
+//        System.out.println("Input matrix is :\n");
+//	displayComEcu(matriz);
 
 	System.out.print("\nThe Inverse is :\n");
 	if (inverse(matriz, inv))
 		display(inv);
 
+
+	
     
     }
     
