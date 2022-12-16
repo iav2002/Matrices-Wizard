dd6aa9ac027693542741f99ff0bd58402fc3ef07
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2022-12-16 18:41:27 +0000

    .

--- src/prueba/doble/PruebaDoble.java
+++ src/prueba/doble/PruebaDoble.java
@@ -55,44 +55,51 @@ import java.util.Scanner;
               sInput = mySC.next();
               myL.getPasswordForLogin(fInput,sInput);
               
-  String role = myL.getRoleForLogin(fInput, sInput); //chequea que role tiene el usuario que acaba de logearse
-        if (role.equals("admin")) {
+            String role = myL.getRoleForLogin(fInput, sInput); //chequea que role tiene el usuario que acaba de logearse
+      
+         if (role.equals("admin")) {
             // Display menu for admins
                  System.out.println("Welcome, admin! Here are your menu options:");
                    System.out.println("1. - Modify your profile");
                    System.out.println("2. - Access a list of all other users in the system");
                    System.out.println("3. - Remove other users from the system.");
                    System.out.println("4. - Review the operations performed by other users");
-                  System.out.println("5. - Salir");
-         int option1Choice  = mySC.nextInt();
-         switch (option1Choice) {
-          case 1 -> {   
-            // crear metodo para poder modificar Admin
-              System.out.println("hola");
-              
-          }
+                  System.out.println("5. - Return to previous menu");
          
-          case 2->{
-              //crear metodo para ver los usuarios sign upeados (llamar desde login system)
-          }
-         
-          case 3->{
-              //poder eliminar usuarios 
-              
-              
-          }
-       
-          case 4->{
-             //ver operaciones hechas por un usuario
-            
-          }
-          case 5->{
-             //salir
-            
-          }
-       }
+         while (true) {
+            int option1Choice  = mySC.nextInt();
+
+            switch (option1Choice) {
+                case 1 -> {   
+                    // crear metodo para poder modificar Admin
+                    System.out.println("hola");
+                }
+                case 2 -> {
+                    //crear metodo para ver los usuarios sign upeados (llamar desde login system)
+                }
+                case 3 -> {
+                    //poder eliminar usuarios 
+                }
+                case 4 -> {
+                    //ver operaciones hechas por un usuario
+                }
+                case 5 -> {
+                    //return to last menu
+                    System.out.println("Back to MatricesWizard Menu!"); 
+                    break;
+                }
+                default -> {
+                    // input is not between 1 and 5, display menu again
+                    System.out.println("Invalid choice, please choose a number between 1 and 5.");
+                }
+            }
+
+            // exit loop if user chose option 5
+            if (option1Choice == 5) {
+                break;
+            }
+}
         
-           
             // ...
       } else if (role.equals("user")) {
             // Display menu for users
@@ -101,7 +108,14 @@ import java.util.Scanner;
                    System.out.println("2. Solve systems of linear equations of two and three variables");
                    System.out.println("3. See the system of equations computed and their solutions");
                    System.out.println("4. Exit");
-            switch (choice) {
+            
+                    
+                   
+         while(true){         
+        int option2Choice  = mySC.nextInt();
+         
+        
+        switch (option2Choice) {
           case 1 -> {   
             // crear metodo para poder modificar Credenciales del user
             
@@ -110,6 +124,16 @@ import java.util.Scanner;
          
           case 2->{
               //Toda la parte de matematicas va aca
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
           }
          
           case 3->{
@@ -119,18 +143,21 @@ import java.util.Scanner;
           }
        
           case 4->{
-             //salir
+           break;
+          
           }
- 
-       }
-            
-                   
+        default -> {
+                    // input is not between 1 and 5, display menu again
+                    System.out.println("Invalid choice, please choose a number between 1 and 4.");
+                }
+         
+         }
+            if (option2Choice == 4) {
+                break;
+            }
+         }         
             // ...
-        } else {
-            // Display error message if role is not recognized
-               System.out.println("Error: Unrecognized role " + role);
-           
-      } 
+        }
         
      }// final de CASE 1 de Matrices Wizzard
         
@@ -155,15 +182,13 @@ import java.util.Scanner;
             }
          
          }
-             
-            
+
+       }//del primer while    
      
-       }
-         
-    
-     }
-    
-  }
+  
+  
+  }//llave del metodo main 
+  }// corchete de la clase
           
 
  
